101aacf1818b8af3a9eee9b42a5c0f9b891114d0
Emilie Brandt <emiliebrandt@gmx.de>
2026-09-22 13:01:06 +0200

    add basic style to homepage and list

--- components/features/SnippetsList/SnippetsList.js
+++ components/features/SnippetsList/SnippetsList.js
@@ -4,14 +4,16 @@ export default function SnippetsList({ snippets }) {
   if (!snippets || snippets.length === 0) {
     return (
       <>
-        <p>No snippets found. Create snippets to display them in a list.</p>
-        <snmall>{`You can create snippets with the "+" button.`}</snmall>
+        <p className="font-semibold">
+          No snippets found. Create snippets to display them in a list.
+        </p>
+        <p className="text-gray-500">{`You can create snippets with the "+" button.`}</p>
       </>
     );
   }
 
   return (
-    <ul>
+    <ul className="grid gap-4">
       {snippets.map((snippet) => {
         return (
           <li key={snippet._id}>
