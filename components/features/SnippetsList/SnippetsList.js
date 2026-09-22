ced67e94f0f6ee2d09b081747252d845d90ead9f
Emilie Brandt <emiliebrandt@gmx.de>
2026-09-22 10:20:07 +0200

    add mapping to SnippetsList

---
+++ components/features/SnippetsList/SnippetsList.js
@@ -0,0 +1,23 @@
+import SnippetCard from "../SnippetCard/SnippetCard";
+
+export default function SnippetsList({ snippets }) {
+  if (!snippets || snippets.length === 0) {
+    return <p>This snippet does not exist. Please try again.</p>;
+  }
+
+  return (
+    <>
+      {snippets.map((snippet) => {
+        return (
+          <SnippetCard
+            key={snippet._id}
+            id={snippet._id}
+            title={snippet.title}
+            language={snippet.language}
+            date={snippet.createdAt}
+          />
+        );
+      })}
+    </>
+  );
+}
