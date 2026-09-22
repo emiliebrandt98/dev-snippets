faed7dd835c74fd2253f9f5f26f62e866d46ee15
Emilie Brandt <emiliebrandt@gmx.de>
2026-09-22 15:29:56 +0200

    add Post to api

--- pages/api/snippets/snippets.js
+++ pages/api/snippets/snippets.js
@@ -23,6 +23,25 @@ export default async function handler(request, response) {
     }
   }
 
+  if (request.method === "POST") {
+    try {
+      const snippetsData = request.body;
+      const newSnippet = await Snippet.create(snippetsData);
+
+      response.status(201).json(newSnippet);
+      return;
+    } catch (error) {
+      console.error(error);
+
+      if (error.name === "ValidationError") {
+        response.status(400).json({ error: error.message });
+        return;
+      }
+      response.status(500).json({ error: "Error creating a snippet." });
+      return;
+    }
+  }
+
   response.status(405).json({ status: "Method not allowed." });
   return;
 }
--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home({ snippets, isLoading, error }) {
           <button
             type="button"
             aria-label="create snippet"
-            className="inline-flex items-center justify-center w-10 h-10 aspect-square rounded-lg bg-gray-100 hover:bg-gray-200"
+            className="fixed bottom-6 right-6 z-50 inline-flex items-center justify-center w-14 h-14 rounded-lg bg-gray-400 hover:bg-gray-500 shadow-lg"
           >
             <Plus size={24} />
           </button>
