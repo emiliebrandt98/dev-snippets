0a98314a6d08159ec6c7b109e6c30cd2d1954861
Emilie Brandt <emiliebrandt@gmx.de>
2026-09-22 10:24:09 +0200

    add content to snippetcard

--- components/features/SnippetCard/SnippetCard.js
+++ components/features/SnippetCard/SnippetCard.js
@@ -1,3 +1,10 @@
-export default function SnippetCard() {
-  return;
+export default function SnippetCard({ title, language, date }) {
+  return (
+    <div>
+      <small>
+        {date} · {language}
+      </small>
+      <h2>{title}</h2>
+    </div>
+  );
 }
