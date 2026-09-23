19d86fdee7b1bfb6feecff50847241c1ee9bcbc2
Emilie Brandt <emiliebrandt@gmx.de>
2026-09-23 11:02:17 +0200

    update api id with PUT method

--- pages/api/snippets/[id].js
+++ pages/api/snippets/[id].js
@@ -23,6 +23,22 @@ export default async function handler(request, response) {
       response.status(200).json(snippet);
       return;
     }
+
+    if (request.method === "PUT") {
+      const snippetData = request.body;
+
+      const snippet = await Snippet.findByIdAndUpdate(id, snippetData, {
+        new: true,
+      });
+
+      if (!snippet) {
+        response.status(404).json({ status: "Error editing Snippet." });
+        return;
+      }
+
+      response.status(200).json({ status: "snippet edited" });
+      return;
+    }
   } catch (error) {
     if (error.name === "ValidationError") {
       response.status(400).json({ error: error.message });
