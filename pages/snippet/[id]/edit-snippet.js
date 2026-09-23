b7ec4635ce15b7b30f91f089845e29dfeb486959
Emilie Brandt <emiliebrandt@gmx.de>
2026-09-23 10:55:27 +0200

    add edit snippet page

---
+++ pages/snippet/[id]/edit-snippet.js
@@ -0,0 +1,42 @@
+import SnippetForm from "@/components/features/SnippetForm/SnippetForm";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
+
+export default function EditSnippetPage({ snippets }) {
+  const router = useRouter();
+  const { id } = router.query;
+
+  async function handleEditSubmit(data) {
+    const response = await fetch(`/api/snippets/${id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      toast.error("Error editing snippet.");
+      return;
+    }
+
+    await mutate(`/api/snippets/${id}`);
+    await mutate("/api/snippets");
+    await router.push("/");
+    toast.success("Your snippet was successfully edited!");
+  }
+  return (
+    <>
+      <div className="max-w-2xl mx-auto p-4">
+        <header className="mb-6">
+          <h1 className="text-2xl font-bold">DevSnippet</h1>
+          <p className="text-gray-500 text-sm">Edit Snippet</p>
+        </header>
+
+        <main>
+          <SnippetForm snippets={snippets} onSubmit={handleEditSubmit} />
+        </main>
+      </div>
+    </>
+  );
+}
