b3c45bc4acf57eb0fb4f1c4291313db723d12dd5
Emilie Brandt <emiliebrandt@gmx.de>
2026-09-24 13:59:17 +0200

    update copy button

--- components/ui/CopyToClipboard/CopyToClipboard.js
+++ components/ui/CopyToClipboard/CopyToClipboard.js
@@ -22,11 +22,7 @@ export default function CopyToClipboard({ textToCopy }) {
       disabled={isCopied}
       className="cursor-pointer inline-flex items-center justify-center w-6 h-6 aspect-square rounded-lg bg-gray-100 hover:bg-gray-200 "
     >
-      {isCopied ? (
-        <Check size={16} className="text-green-600" />
-      ) : (
-        <Copy size={16} />
-      )}
+      {isCopied ? <Check size={16} /> : <Copy size={16} />}
     </button>
   );
 }
