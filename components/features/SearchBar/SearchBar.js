935d6147b12fc3067f60489d37cf066db2f33dcc
Emilie Brandt <emiliebrandt@gmx.de>
2026-09-25 22:49:31 +0200

    add clean function to input

--- components/features/SearchBar/SearchBar.js
+++ components/features/SearchBar/SearchBar.js
@@ -1,6 +1,9 @@
-import { Search } from "lucide-react";
+import { Search, X } from "lucide-react";
 
 export default function SearchBar({ search, onSearch }) {
+  function handleRemoveSearchValue() {
+    return onSearch("");
+  }
   return (
     <div className="flex flex-row h-10 w-full items-center gap-2 px-3 rounded-lg bg-gray-100 hover:bg-gray-200 focus-within:ring-1 focus-within:ring-black">
       <Search size={16} className="text-gray-500 shrink-0" />
@@ -13,6 +16,13 @@ export default function SearchBar({ search, onSearch }) {
         className="bg-transparent outline-none text-sm w-full"
         placeholder="Search"
       />
+      <button
+        onClick={handleRemoveSearchValue}
+        type="button"
+        className="cursor-pointer"
+      >
+        <X size={16} />
+      </button>
     </div>
   );
 }
