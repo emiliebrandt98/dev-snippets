46fd734c14be50983467c1a5e03000c77fdb14bc
Emilie Brandt <emiliebrandt@gmx.de>
2026-09-25 10:50:40 +0200

    add api route for tag

---
+++ pages/api/tag.js
@@ -0,0 +1,58 @@
+import dbConnect from "@/db/connect";
+import Tag from "@/db/models/Tag";
+
+export default async function handler(request, response) {
+  try {
+    await dbConnect();
+  } catch (error) {
+    response.status(500).json({ error: "Database connection failed." });
+    return;
+  }
+
+  if (request.method === "GET") {
+    try {
+      const tags = await Tag.find();
+      response.status(200).json(tags);
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ error: "Error retrieving the tags." });
+      return;
+    }
+  }
+
+  if (request.method === "POST") {
+    try {
+      const tagsData = request.body;
+      const newTag = await Tag.create(tagsData);
+
+      response.status(201).json(newTag);
+      return;
+    } catch (error) {
+      console.error(error);
+
+      if (error.name === "ValidationError") {
+        response.status(400).json({ error: error.message });
+        return;
+      }
+      response.status(500).json({ error: "Error creating a tag." });
+      return;
+    }
+  }
+
+  if (request.method === "DELETE") {
+    try {
+      const { tagIds } = request.body;
+
+      const deletedTag = await Tag.findByIdAndDelete(tagIds);
+      response.status(200).json(deletedTag);
+      return;
+    } catch (error) {
+      console.error(error);
+      response.status(400).json({ error: error.message });
+      return;
+    }
+  }
+
+  response.status(405).json({ status: "Method not allowed." });
+  return;
+}
