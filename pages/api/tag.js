5b461079b0bb42854147f00de18b76afb349e121
Emilie Brandt <emiliebrandt@gmx.de>
2026-09-25 17:17:29 +0200

    add a return to api tag and language

--- pages/api/tag.js
+++ pages/api/tag.js
@@ -13,6 +13,7 @@ export default async function handler(request, response) {
     try {
       const tags = await Tag.find().sort({ createdAt: -1 });
       response.status(200).json(tags);
+      return;
     } catch (error) {
       console.error(error);
       response.status(500).json({ error: "Error retrieving the tags." });
